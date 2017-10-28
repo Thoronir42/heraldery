df4c1de73c0152121b9c3a367432f82a830f067a
vamjan <janvampol@gmail.com>
2017-10-28 23:55:51 +0200

    Created a simple SVG loader.

--- Rendering/CrestRenderer.cs
+++ Rendering/CrestRenderer.cs
@@ -10,6 +10,14 @@ namespace Heraldry.Rendering
 {
     class CrestRenderer
     {
+        private SvgLoader Loader { get; set; }
+
+        public CrestRenderer() : this(new SvgLoader(400, 400)) { /* default constructor */ }
+
+        public CrestRenderer(SvgLoader loader) {
+            this.Loader = loader;
+        }
+
         public void Render(BlazonInstance blazon) {
             // todo: return image
         }
---
+++ Rendering/SVGLoader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Svg;
+
+namespace Heraldry.Rendering {
+
+
+    class SvgLoader {
+
+        private int Height { get; set; }
+        private int Width { get; set; }
+
+        private string SvgDirectory {
+            get {
+                return Environment.CurrentDirectory + "\\resources\\rendering\\";
+            }
+        }
+
+        public int Loaded { get; private set; } = 0;
+
+        public SvgLoader(int heigth, int width) {
+            this.Height = heigth;
+            this.Width = width;
+        }
+
+        public SvgDocument GetSvgFromFile(string name) {
+            SvgDocument retval = null;
+            if (File.Exists(SvgDirectory + name)) {
+                retval = SvgDocument.Open(SvgDirectory + name);
+                Loaded++;
+            } else {
+                throw new FileNotFoundException("File " + SvgDirectory + " " + name + " is missing.");
+                //TODO: might not be mandatory to kill the app here
+            }
+            return AdjustSize(retval);
+        }
+
+        /// <summary>
+        /// Check aspect ratio and adjust size.
+        /// TODO: check more picture properties
+        /// </summary>
+        /// <param name="document"></param>
+        /// <returns>Picture with adjusted maximum size with same aspect ratio as input.</returns>
+        private SvgDocument AdjustSize(SvgDocument document) {
+            if (document.Height > this.Height) {
+                document.Width = (int)((document.Width / (double)document.Height) * this.Height);
+                document.Height = this.Height;
+            }
+            return document;
+        }
+    }
+}
