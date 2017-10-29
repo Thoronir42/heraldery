64e6598c0f12bbe4d315e818e772460a3416b617
vamjan <janvampol@gmail.com>
2017-10-29 23:11:06 +0100

    Added .sln files to .gitignore and experimented with image output

--- Rendering/CrestRenderer.cs
+++ Rendering/CrestRenderer.cs
@@ -4,7 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 using System.Threading.Tasks;
+using Svg;
 
 namespace Heraldry.Rendering
 {
@@ -12,14 +14,21 @@ namespace Heraldry.Rendering
     {
         private SvgLoader Loader { get; set; }
 
-        public CrestRenderer() : this(new SvgLoader(400, 400)) { /* default constructor */ }
+        public CrestRenderer() : this(new SvgLoader(100, 100)) { /* default constructor */ }
 
         public CrestRenderer(SvgLoader loader) {
             this.Loader = loader;
         }
 
-        public void Render(BlazonInstance blazon) {
-            // todo: return image
+        public Bitmap Render(BlazonInstance blazon) {
+            SvgDocument tmp = Loader.GetSvgFromFile("iconmonstr-shield-1.svg");
+            tmp.Children[0].Fill = new SvgColourServer(Color.Red);
+            return GetBitmapFromSvg(tmp);
+        }
+
+        private Bitmap GetBitmapFromSvg(SvgDocument input) {
+            Bitmap retval = input.Draw(100, 100);
+            return retval;
         }
     }
 }
